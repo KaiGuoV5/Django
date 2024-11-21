e9c67a833b28e546b0aecb6ce0a987210f80335b
kaiguov5 <kaiguov5@gmail.com>
2024-11-21 20:28:13 +0800

    feat(framework): Change datebase form sqlite3 to mysql

--- DevOps/settings.py
+++ DevOps/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,22 @@ WSGI_APPLICATION = 'DevOps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tsingmicro',
+        'USER': 'root',
+        'PASSWORD': '123.com',
+        'HOST': '192.168.120.17',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
